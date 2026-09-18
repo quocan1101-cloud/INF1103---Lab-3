f4511b7e13d72382b9eb61c8f8e4562979aba101
Adam <quocan1101@gmail.com>
2026-09-18 16:54:28 +0800

    Get valid input function

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,1 +1,19 @@
 inventory = 0
+
+def get_valid_input():
+    input_value = input("Enter the number of items to add to inventory (or type 'quit' to exit): ")
+    if input_value == "quit":
+        return "quit"
+
+    try:
+        val = int(input_value)
+    except ValueError:
+        print("Please enter a valid number or 'quit' to exit.")
+        return None
+
+    if val < 0:
+        print("Please enter a positive number.")
+        return None
+
+    return val
+
