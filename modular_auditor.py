14d04bd59937bd11775fee0b7d5dbd67d9bbec00
Adam <quocan1101@gmail.com>
2026-09-20 16:17:29 +0800

    Process delivery function

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,4 +1,4 @@
-inventory = 0
+tax_rate = 0.10
 
 def get_valid_input():
     input_value = input("Enter the number of items to add to inventory (or type 'quit' to exit): ")
@@ -17,3 +17,21 @@ def get_valid_input():
 
     return val
 
+def process_delivery(current_total, new_value):
+    return current_total + new_value
+
+def main():
+    inventory = 0
+
+    while True:
+        value = get_valid_input()
+
+        if value == "quit":
+            break
+        if value is None:
+            continue
+
+        inventory = process_delivery(inventory, value)
+        print (f"Current inventory: {inventory}")
+
+main()
